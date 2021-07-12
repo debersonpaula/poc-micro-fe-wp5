24b2344cbd1872106147383e837bd14cc4a65573
Deberson de Paula <deberson.paula@gmail.com>
2021-07-12 08:51:30 -0300

    initial config

---
+++ webpack.default.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
+
+module.exports = ({ appName, port, remotes = null }) => ({
+  entry: "./src/index.ts",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: port,
+  },
+  output: {
+    publicPath: "auto",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|mjs|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react", "@babel/preset-typescript"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: appName,
+      remotes: remotes || {},
+      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
+    }),
+    new ExternalTemplateRemotesPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+});
