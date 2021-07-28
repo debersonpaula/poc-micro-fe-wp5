0cdd06c319ea32977ad59cb9dc186d4148161241
Deberson de Paula <deberson.paula@gmail.com>
2021-07-28 10:38:06 -0300

    added mod-about

---
+++ mod-about/config.js
@@ -0,0 +1,5 @@
+const configurator = require("../react-config");
+const appName = "modAbout";
+const port = 4003;
+
+module.exports = configurator({ appName, port, expose: true });
---
+++ mod-about/src/interfaces.ts
@@ -0,0 +1,5 @@
+import React from "react";
+
+export interface IAppProps {}
+
+export type AppType = React.FC<IAppProps>;
